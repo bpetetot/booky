06f80f9be004e3a8cf45a3aa0f0f4cd7a94bb031
Benjamin Petetot <bpetetot@gmail.com>
2017-08-05 13:45:36 +0200

    :sparkles: add google books provider

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # booky
+
+## environement variables
+
+Define following environement variables or add them to a `.env` file on project root.
+```bash
+GOOGLE_BOOKS_API_KEY=<GOOGLE_BOOKS_API_KEY>
+```
--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node --harmony
-console.log('booky server')
+require('dotenv').config()
+
+const gbooks = require('./providers/gbooks')
+
+process.on('unhandledRejection', r => console.log(r))
+
+const start = async () => {
+  const result = await gbooks.fetchBook('isbn:9782205049657', 'fr')
+
+  result.items.forEach(e => console.log(e.volumeInfo.title))
+}
+
+start()
