d3d71722f2fdd87bd3309e86c5c1a2bce5644680
Benjamin Petetot <bpetetot@gmail.com>
2017-08-07 17:29:46 +0200

    :sparkles: connect gui and server

--- gui/src/app.jsx
+++ gui/src/app.jsx
@@ -8,11 +8,12 @@ import './app.css'
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { scanned: false, code: undefined }
+    this.state = { scanned: true, code: undefined, book: undefined }
     this.handleDetect = this.handleDetect.bind(this)
     this.changeCode = this.changeCode.bind(this)
     this.rescan = this.rescan.bind(this)
     this.stopScan = this.stopScan.bind(this)
+    this.fetchBook = this.fetchBook.bind(this)
   }
 
   handleDetect({ codeResult }) {
@@ -20,38 +21,51 @@ class App extends Component {
   }
 
   changeCode(code) {
-    this.setState({ scanned: false, code })
+    this.setState({ scanned: true, code })
   }
 
   rescan() {
-    this.setState({ scanned: true, code: undefined })
+    this.setState({ scanned: false, code: undefined })
   }
 
   stopScan() {
-    this.setState({ scanned: false })
+    this.setState({ scanned: true })
+  }
+
+  async fetchBook() {
+    const response = await fetch(`/api/books/${this.state.code}`)
+    const book = await response.json()
+    this.setState(() => ({ book }))
   }
 
   render() {
-    const { scanned, code } = this.state
+    const { scanned, code, book } = this.state
     return (
       <div>
         <Navbar logo="📚 Booky" />
         <div className="app-content">
           <Panel title="ISBN Scanner">
-            {scanned &&
+            {!scanned &&
+              !book &&
               <div>
                 <Scanner onDetect={this.handleDetect} />
                 <input type="button" defaultValue="close" onClick={this.stopScan} />
               </div>}
-            {!scanned &&
+            {scanned &&
+              !book &&
               <div>
                 <input
                   type="text"
                   defaultValue={code}
                   onChange={e => this.changeCode(e.target.value)}
                 />
-                <input type="button" defaultValue="validate" />
+                <input type="button" defaultValue="validate" onClick={this.fetchBook} />
                 <input type="button" defaultValue="scan" onClick={this.rescan} />
+              </div>}
+            {book &&
+              <div>
+                {book.title}
+                {book.cover && book.cover.medium && <img src={book.cover.medium} alt="cover" />}
               </div>}
           </Panel>
         </div>
--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ const PORT = '3001'
 const init = () => {
   const app = new Koa()
 
-  app.use(route.get('/books/:isbn', books.fetch))
+  app.use(route.get('/api/books/:isbn', books.fetch))
 
   app.listen(PORT)
   console.log(`listening on port ${PORT}`)
